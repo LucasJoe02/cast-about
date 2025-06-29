3e4b6fac243b6b7a8b864d2f4925dd62da04c150
Lucas Redding <lre61@uclive.ac.nz>
2025-06-29 22:04:38 +0100

    Separate files and direction is maintained on stopping

--- angler.js
+++ angler.js
@@ -1,35 +1,62 @@
-class Angler extends Sprite {
+class Angler {
     static MAX_VELOCITY = 2;
 
-	constructor(x, y, diameter) {
-		super(x, y);
-		this.diameter = diameter;
-	}
-	
-	update() {
-		this.drag = 3;
+    constructor(x, y, diameter) {
+        this.trueDirection = 0;
+        this.sprite = new Sprite();
+        this.sprite.x = x;
+        this.sprite.y = y;
+        this.sprite.diameter = diameter;
+        this.rod = new Sprite();
+        this.rod.width = 70;
+        this.rod.height = 10;
+        this.rod.physics = NONE;
+        this.rod.offset.x = 15;
+    }
+
+    update() {
+        if (this.sprite.speed < 0.1 && this.sprite.speed > 0) {
+            this.trueDirection = this.sprite.direction;
+            this.sprite.speed = 0;
+        } else if (this.sprite.speed > 0.1) {
+            this.trueDirection = this.sprite.direction;
+        } else {
+            this.sprite.direction = this.trueDirection;
+        }
+        this.sprite.drag = 3;
         let forceX = 0;
         let forceY = 0;
         let dt = deltaTime / 1000;
 
-        if (kb.pressing('up'))    forceY -= 1;
-        if (kb.pressing('down'))  forceY += 1;
-        if (kb.pressing('left'))  forceX -= 1;
+        if (kb.pressing('up')) forceY -= 1;
+        if (kb.pressing('down')) forceY += 1;
+        if (kb.pressing('left')) forceX -= 1;
         if (kb.pressing('right')) forceX += 1;
 
         if (forceX !== 0 || forceY !== 0) {
-            this.drag = 0;
+            this.sprite.drag = 0;
 
             let length = Math.hypot(forceX, forceY);
             forceX /= length;
             forceY /= length;
 
             let forceMagnitude = 1000;
-            this.applyForce(forceX * forceMagnitude * dt, forceY * forceMagnitude * dt);
+            this.sprite.applyForce(forceX * forceMagnitude * dt, forceY * forceMagnitude * dt);
         }
 
-        if (this.speed > Angler.MAX_VELOCITY) {
-            this.speed = Angler.MAX_VELOCITY;
+        if (this.sprite.speed > Angler.MAX_VELOCITY) {
+            this.sprite.speed = Angler.MAX_VELOCITY;
         }
-	}
+
+        let angle = this.trueDirection;
+        let offsetDistance = this.sprite.diameter / 2;
+
+        let rightX = cos(angle + 90) * offsetDistance;
+        let rightY = sin(angle + 90) * offsetDistance;
+
+        this.rod.x = this.sprite.x + rightX;
+        this.rod.y = this.sprite.y + rightY;
+
+        this.rod.rotation = this.trueDirection;
+    }
 }
--- sketch.js
+++ sketch.js
@@ -4,57 +4,13 @@ new Canvas();
 displayMode(CENTER, PIXELATED, 4);
 world.gravity.y = 0;
 
-let angler = new Sprite();
-angler.x = halfWidth;
-angler.y = halfHeight;
-angler.diameter = 50;
-MAX_VELOCITY = 2;
-
-let rod = new Sprite();
-rod.width = 70;
-rod.height = 10;
-rod.physics = NONE;
+let angler = new Angler(halfWidth, halfHeight, 50);
 
 function update() {
-	rod.debug = true;
 	background('skyblue');
-	textSize(32);
+    angler.update();
 
-	angler.drag = 3;
-	let forceX = 0;
-	let forceY = 0;
-	let dt = deltaTime / 1000;
-
-	if (kb.pressing('up'))    forceY -= 1;
-	if (kb.pressing('down'))  forceY += 1;
-	if (kb.pressing('left'))  forceX -= 1;
-	if (kb.pressing('right')) forceX += 1;
-
-	if (forceX !== 0 || forceY !== 0) {
-		angler.drag = 0;
-
-		let length = Math.hypot(forceX, forceY);
-		forceX /= length;
-		forceY /= length;
-
-		let forceMagnitude = 1000;
-		angler.applyForce(forceX * forceMagnitude * dt, forceY * forceMagnitude * dt);
-	}
-
-	if (angler.speed > MAX_VELOCITY) {
-		angler.speed = MAX_VELOCITY;
-	}
-
-	let angle = angler.direction;
-	let offsetDistance = angler.diameter / 2;
-
-	let rightX = cos(angle + 90) * offsetDistance;
-	let rightY = sin(angle + 90) * offsetDistance;
-
-	rod.x = angler.x + rightX;
-	rod.y = angler.y + rightY;
-
-	rod.rotation = angler.direction;
-	text(angle, 12, 60);
+    textSize(32);
+	text(angler.sprite.direction, 12, 60);
 
 }
