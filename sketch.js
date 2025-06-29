d3d1659a1aeb77ea5f7d8fc635721906743f72b0
Lucas Redding <lre61@uclive.ac.nz>
2025-06-29 21:18:11 +0100

    Angler movement and rod

--- sketch.js
+++ sketch.js
@@ -4,7 +4,11 @@ new Canvas();
 displayMode(CENTER, PIXELATED, 4);
 world.gravity.y = 0;
 
-let angler = new Angler(halfWidth, halfHeight, 50);
+let angler = new Sprite();
+angler.x = halfWidth;
+angler.y = halfHeight;
+angler.diameter = 50;
+MAX_VELOCITY = 2;
 
 let rod = new Sprite();
 rod.width = 70;
@@ -16,7 +20,30 @@ function update() {
 	background('skyblue');
 	textSize(32);
 
-	
+	angler.drag = 3;
+	let forceX = 0;
+	let forceY = 0;
+	let dt = deltaTime / 1000;
+
+	if (kb.pressing('up'))    forceY -= 1;
+	if (kb.pressing('down'))  forceY += 1;
+	if (kb.pressing('left'))  forceX -= 1;
+	if (kb.pressing('right')) forceX += 1;
+
+	if (forceX !== 0 || forceY !== 0) {
+		angler.drag = 0;
+
+		let length = Math.hypot(forceX, forceY);
+		forceX /= length;
+		forceY /= length;
+
+		let forceMagnitude = 1000;
+		angler.applyForce(forceX * forceMagnitude * dt, forceY * forceMagnitude * dt);
+	}
+
+	if (angler.speed > MAX_VELOCITY) {
+		angler.speed = MAX_VELOCITY;
+	}
 
 	let angle = angler.direction;
 	let offsetDistance = angler.diameter / 2;
